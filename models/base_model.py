5eef4ff73bea8d8e96fcc42d30404a45ed2d7142
anaruzz <laabidigh@gmail.com>
2020-05-03 04:17:52 +0100

    Add BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module that defines the class BaseModel
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """
+    Class that defines common attributes and methods for
+    the other classes
+    """
+    def __init__(self):
+        """
+        BaseModel instance constructor
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        ncl = self.__class__.__name__
+        return '[' + ncl + '] (' + self.id + ' ' + str(self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dic = {}
+        for key, value in self.__dict__.items():
+            if key in ['created_at', 'updated_at']:
+                dic[key] = value.isoformat()
+            else:
+                dic[key] = value
+        dic['__class__'] = self.__class__.__name__
+        return dic
