f6e9132b6e3b3539327e2cd62cf63998aaf74343
anaruzz <laabidigh@gmail.com>
2020-05-04 14:29:58 +0100

    Console 1.0

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ from cmd import Cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 import json
 
 
@@ -15,7 +20,7 @@ class HBNBCommand(Cmd):
     entry point hbnb class
     """
     prompt = "(hbnb) "
-    all_classes = ["BaseModel", "User", "Place",\
+    all_classes = ["BaseModel", "User", "Place",
                     "State", "City", "Amenity", "Review"]
 
     def do_quit(self, arg):
